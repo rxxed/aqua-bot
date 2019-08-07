36e6ee047ea638dde8460a9fd552fccf0d6c1d53
Raaed Shaikh <sraaed1908@gmail.com>
2019-08-07 15:33:21 +0530

    added setInterval command

--- aquabot.py
+++ aquabot.py
@@ -2,6 +2,8 @@ import requests, urllib
 import json
 import time, random
 
+# inspect.cleandoc to remove tabs from multiline strings
+import inspect
 
 from messages_list import messages_list
 
@@ -9,6 +11,10 @@ from messages_list import messages_list
 # telegram API key
 BOT_TOKEN = ""
 BASE_URL = f"https://api.telegram.org/bot{BOT_TOKEN}/"
+
+
+# default 1 hour interval time between notifications
+interval_seconds = 3600
 
 
 # this function returns the response from a url
@@ -75,10 +81,39 @@ def handle_user_messages(updates):
         try:
             text = update["message"]["text"]
             chatid = update["message"]["chat"]["id"]
-            if text == 'stone free':
-                send_message(text, chatid)
+            if is_under_command:
+                if command_name == 'setInterval':
+                    if text.isdigit():
+                        interval_seconds = text
+                    else:
+                        send_message("Not a number")
+                is_under_command = False
+            else:
+                if text == '/setInterval':
+                    is_under_command = True
+                    command_name = 'setInterval'
+                    set_interval()
+                else:
+                    message = """I was unable to comprehend your command.
+                              Try one of these following commands instead: \n
+                              /setInterval : You can use this command to change
+                              the interval between each notification. \n"""
+                    message = inspect.cleandoc(message)
+                    send_message(message, chatid)
+
         except Exception as e:
             print(e)
+
+
+def set_interval():
+    chatid = get_chat_id(get_updates())
+    message = "How many seconds of an interval do you want between each notification?"
+    send_message(message, chatid)
+    time.sleep(10)
+    latest_text_and_id = get_latest_chat_id_and_text(get_updates())
+    interval = latest_text_and_id[0]
+    message = f"So you want an interval of {interval} seconds"
+    send_message(message, chatid)
 
 
 def notify_user(interval):
@@ -101,7 +136,8 @@ def main():
         updates = get_updates(last_update_id)
         if len(updates["result"]) > 0:
             last_update_id = get_latest_update_id(updates) + 1
-            handle_user_messages(updates)
+            is_under_command = False
+            handle_user_messages(updates, is_under_command)
 
 
 if __name__ == '__main__':
