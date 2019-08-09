4ffed83fad381e17762ca12e6df84c9514fb5884
Raaed Shaikh <sraaed1908@gmail.com>
2019-08-09 19:25:53 +0530

    added more messages

--- messages_list.py
+++ messages_list.py
@@ -1,4 +1,5 @@
 messages_list = ["Drink water", "Did you drink water?",
              "How about one more glass of water?",
+             "AquaBot commands, thee shall imbibe a chalice of water"
              "Drink water for 10 nanoseconds of good luck",
              "Your body desires water", "H2O time motherf*cker"]
